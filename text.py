a06bffda8c95be86b18d03f9cb45aea0a327549c
Jake Celentano <jakecelentano@gmail.com>
2023-02-23 17:02:01 -0500

    test

--- text.py
+++ text.py
@@ -13,8 +13,9 @@ class ShowText(SampleBase):
         font = graphics.Font()
         font.LoadFont("fonts/7x13.bdf")
         textColor = graphics.Color(255, 255, 0)
-        pos = 50
         my_text = self.args.text
+        x = 50
+        y = 80
 
         words_list = ['apple', 
                       'banana',
@@ -26,15 +27,17 @@ class ShowText(SampleBase):
                       'blueberry',
                       'raspberry',
                       'blackberry']
-
+        
         for word in words_list:
             my_text = word
             offscreen_canvas.Clear()
             # DrawText(Canvas, Font, x, y, color, text);
-            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+            len = graphics.DrawText(offscreen_canvas, font, x, y, textColor, my_text)
             print(len)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
             time.sleep(5)
+            x += 10
+            y += 10
 
 
 
