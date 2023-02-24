da8c730bc654d9226419b907e6c3cc969a3fe8dc
Jake <jake.celentano@galleysolutions.co>
2023-02-24 03:51:39 -0500

    test

--- nhl-matrix/code/runtext copy.py
+++ nhl-matrix/code/runtext copy.py
@@ -12,15 +12,21 @@ class RunText(SampleBase):
 
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
-        year = datetime.datetime.now().year
-        nhl = NHL(year)
-        print(year)
-        for team in NHL_TEAMS:
-            print(team)
-            team = nhl.get_team_by_name(team)
-            self.drawBorder(offscreen_canvas)
-            self.drawLogo(offscreen_canvas, team=team)
-            time.sleep(15)
+        font = graphics.Font()
+        font.LoadFont("fonts/7x13.bdf")
+        textColor = graphics.Color(255, 255, 0)
+        pos = offscreen_canvas.width
+        my_text = self.args.text
+
+        while True:
+            offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+            pos -= 1
+            if (pos + len < 0):
+                pos = offscreen_canvas.width
+
+            time.sleep(0.05)
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
 # Main function
