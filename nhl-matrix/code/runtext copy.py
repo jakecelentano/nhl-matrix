50abcddd1a2cb5cc2a97f0b7b1c18a4c981237ad
Jake <jake.celentano@galleysolutions.co>
2023-02-24 03:55:18 -0500

    test

--- nhl-matrix/code/runtext copy.py
+++ nhl-matrix/code/runtext copy.py
@@ -3,6 +3,8 @@
 from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
+from nhl import NHL
+from team import Team
 
 
 class RunText(SampleBase):
@@ -11,22 +13,24 @@ class RunText(SampleBase):
         self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
 
     def run(self):
+        year = 2023
+        nhl = NHL(year)
+        team = Team(nhl, 'Boston Bruins')
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
         font.LoadFont("fonts/7x13.bdf")
         textColor = graphics.Color(255, 255, 0)
         pos = offscreen_canvas.width
-        my_text = self.args.text
-
-        while True:
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
-
-            time.sleep(0.05)
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+        my_text = self.args.text    
+        try:
+            logo = team.get_logo()
+        except:
+            logo = 'images/nhl.png'
+        x = 5
+        y = 5
+        offscreen_canvas.SetImage(logo, x, y)
+        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+        time.sleep(10)
 
 
 # Main function
