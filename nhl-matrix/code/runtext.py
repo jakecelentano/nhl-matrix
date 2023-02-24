03f3952004e6b034b3266041a87237dd2d94e91e
Jake <jake.celentano@galleysolutions.co>
2023-02-24 18:42:54 -0500

    test image

--- nhl-matrix/code/runtext.py
+++ nhl-matrix/code/runtext.py
@@ -15,13 +15,15 @@ class RunText(SampleBase):
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-        font.LoadFont("fonts/7x13.bdf")
+        font.LoadFont("fonts/4x16.bdf")
         textColor = graphics.Color(255, 255, 0)
         pos = offscreen_canvas.width
         my_text = self.args.text
         year = 2023
         nhl = NHL(year)
         bruins = nhl.get_team_by_name("Boston Bruins")
+        self.drawLogo(offscreen_canvas, team=bruins)
+        time.sleep(15)
         next_games = bruins.get_next_games(2)
         next_game = next_games[0]
         my_text = next_game.pretty_print_string()
@@ -35,6 +37,15 @@ class RunText(SampleBase):
 
             time.sleep(0.05)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+    
+    def drawLogo(self, offscreen_canvas, x=0, y=4, team=None):
+        # offscreen_canvas.Clear()
+        try:
+            logo = team.get_logo()
+        except:
+            logo = 'images/nhl.png'
+        offscreen_canvas.SetImage(logo, x, y)
+        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
 # Main function
