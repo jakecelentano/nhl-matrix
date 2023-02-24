61202ca668b245b72193631fea3b15fb6ff4eab9
Jake <jake.celentano@galleysolutions.co>
2023-02-24 03:22:17 -0500

    setup

--- nhl-matrix/code/setup.py
+++ nhl-matrix/code/setup.py
@@ -31,3 +31,11 @@ setup(
     ext_modules         = [core_ext, graphics_ext],
     packages            = ['rgbmatrix']
 )
+
+STATS_API_PREFIX = "https://statsapi.web.nhl.com"
+LOGO_API_PREFIX = "https://www-league.nhlstatic.com/images/logos/teams-current-primary-light"
+
+OBSERVE_DAYLIGHT_SAVINGS = True
+STANDARD_TIMEZONE = "EST"
+DST_TIMEZONE = "EDT"
+NHL_TEAMS = ["Boston Bruins"]
