20f1f2e68b8ce60af40838c4ab028a41df2c397d
Jake <jake.celentano@galleysolutions.co>
2023-02-24 19:25:00 -0500

    border

--- nhl-matrix/code/runtext.py
+++ nhl-matrix/code/runtext.py
@@ -8,6 +8,9 @@ from team import Team
 from PIL import Image
 
 
+BRUINS_YELLOW = graphics.Color(253, 185, 39)
+BRUINS_BLACK = graphics.Color(0, 0, 0)
+
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
@@ -24,6 +27,7 @@ class RunText(SampleBase):
         nhl = NHL(year)
         bruins = nhl.get_team_by_name("Boston Bruins")
         self.drawLogo(offscreen_canvas, team=bruins)
+        self.drawBorder(offscreen_canvas)
         time.sleep(15)
         
         #next_games = bruins.get_next_games(2)
@@ -40,12 +44,19 @@ class RunText(SampleBase):
         #    time.sleep(0.05)
         #    offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
     
-    def drawLogo(self, offscreen_canvas, x=0, y=4, team=None):
+    def drawLogo(self, offscreen_canvas, x=4, y=4, team=None):
         logo = 'logos/Boston Bruins.png'
         image = Image.open(logo)
-        image.thumbnail((64, 64), Image.ANTIALIAS)
+        image.thumbnail((56, 56), Image.ANTIALIAS)
         image = image.convert('RGB')
         offscreen_canvas.SetImage(image, x, y)
+        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+
+    def drawBorder(self, offscreen_canvas):
+        graphics.DrawLine(offscreen_canvas, 0, 0, 63, 0, BRUINS_YELLOW)
+        graphics.DrawLine(offscreen_canvas, 0, 0, 0, 63, BRUINS_YELLOW)
+        graphics.DrawLine(offscreen_canvas, 63, 0, 63, 63, BRUINS_YELLOW)
+        graphics.DrawLine(offscreen_canvas, 0, 63, 63, 63, BRUINS_YELLOW)
         offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
