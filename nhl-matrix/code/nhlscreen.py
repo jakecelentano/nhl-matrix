ceebd74ee9f1ea87b18c99cf15229e3486253bcb
Jake <jake.celentano@galleysolutions.co>
2023-03-03 12:57:13 -0500

    test

--- nhl-matrix/code/nhlscreen.py
+++ nhl-matrix/code/nhlscreen.py
@@ -72,10 +72,10 @@ class NHLScreen(SampleBase):
         away_team_logo = away_team_logo.convert('RGB')
         # paste logos onto canvas
         offscreen_canvas.SetImage(home_team_logo, x, y)
-        offscreen_canvas.SetImage(away_team_logo, x+30, y)
+        offscreen_canvas.SetImage(away_team_logo, x+38, y)
 
         # draw vs between logos
-        graphics.DrawText(offscreen_canvas, font, x+30, y+24, color, "vs")
+        graphics.DrawText(offscreen_canvas, font, x+24, y+24, color, "vs")
 
         #game_time = game.get_game_time()
         #graphics.DrawText(offscreen_canvas, font, x, y+30, color, game_time)
