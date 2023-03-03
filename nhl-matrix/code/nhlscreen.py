872337b130e7c2833107f39ef0169ee8dbc9f427
Jake <jake.celentano@galleysolutions.co>
2023-03-03 12:26:42 -0500

    rename

--- nhl-matrix/code/nhlscreen.py
+++ nhl-matrix/code/nhlscreen.py
@@ -1,70 +1,60 @@
+#!/usr/bin/env python
 from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
 from nhl import NHL
 from team import Team
-from PIL import Image
 import datetime
-from config import DEFAULT_FONT, DEFAULT_FONT_COLOR, DEFAULT_TEAM
-from matrix import MATRIX
+from config import DEFAULT_TEAM
 
 
 
+class NHLScreen(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(NHLScreen, self).__init__(*args, **kwargs)
 
-class NHLScreen():
-    def __init__(self):
-        self.nhl = NHL(str(datetime.datetime.now().year))
-        self.matrix = MATRIX
-        self.color = graphics.Color(DEFAULT_FONT_COLOR[0], DEFAULT_FONT_COLOR[1], DEFAULT_FONT_COLOR[2])
+    # main function
+    def run(self):
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        nhl = NHL(str(datetime.datetime.now().year))
+        team = nhl.get_team_by_name(DEFAULT_TEAM)
+        team_primary_color = team.get_primary_color()
+        print(team_primary_color)
+        color = graphics.Color(team_primary_color[0], team_primary_color[1], team_primary_color[2])
+        while True:
+            self.drawBorder(color)
+            time.sleep(5)
 
-        
+    
+    def drawBorder(self, color):
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        graphics.DrawLine(offscreen_canvas, 0, 0, 63, 0, color)
+        graphics.DrawLine(offscreen_canvas, 0, 0, 0, 63, color)
+        graphics.DrawLine(offscreen_canvas, 63, 0, 63, 63, color)
+        graphics.DrawLine(offscreen_canvas, 0, 63, 63, 63, color)
+        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+
     def drawUpcomingGamesScreen(self, team):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-        font.LoadFont(DEFAULT_FONT)
-        
-        x = 2
-        y = 2   
-        team_color = graphics.Color(team.get_primary_color()[0], team.get_primary_color()[1], team.get_primary_color()[2])
-        
+        font.LoadFont("fonts/7x13.bdf")
+        textColor = graphics.Color(255, 255, 0)
+        pos = offscreen_canvas.width
+        my_text = self.args.text
 
-        games = team.get_next_games(2)
+        while True:
+            offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+            pos -= 1
+            if (pos + len < 0):
+                pos = offscreen_canvas.width
 
-        for game in games:
-            home_team = self.nhl.get_team_by_id(game.get_game_home_team_id())
-            away_team = self.nhl.get_team_by_id(game.get_game_away_team_id())
-            # get logo paths
-            home_team_logo = home_team.get_logo()
-            away_team_logo = away_team.get_logo()
-            # convert to 30x30 PIL images in RGB
-            home_team_logo = Image.open(home_team_logo)
-            home_team_logo.thumbnail((30, 30), Image.ANTIALIAS)
-            home_team_logo = home_team_logo.convert('RGB')
-            away_team_logo = Image.open(away_team_logo)
-            away_team_logo.thumbnail((30, 30), Image.ANTIALIAS)
-            away_team_logo = away_team_logo.convert('RGB')
-            # paste logos onto canvas
-            offscreen_canvas.SetImage(home_team_logo, x, y)
-            offscreen_canvas.SetImage(away_team_logo, x+30, y)
-            # draw team names
-            graphics.DrawText(offscreen_canvas, font, x, y+30, team_color, home_team.get_name())
-            graphics.DrawText(offscreen_canvas, font, x+30, y+30, team_color, away_team.get_name())
-            # draw game time
-            game_time = game.get_game_time()
-            graphics.DrawText(offscreen_canvas, font, x, y+45, team_color, game_time)
-            y += 30
-        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-
-    def drawBorder(self):
-        graphics.DrawLine(offscreen_canvas, 0, 0, 63, 0, self.color)
-        graphics.DrawLine(offscreen_canvas, 0, 0, 0, 63, self.color)
-        graphics.DrawLine(offscreen_canvas, 63, 0, 63, 63, self.color)
-        graphics.DrawLine(offscreen_canvas, 0, 63, 63, 63, self.color)
-        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            time.sleep(0.05)
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
-
-
-    
-
-
+# Main function
+if __name__ == "__main__":
+    screen = NHLScreen()
+    if (not screen.process()):
+        screen.print_help()
