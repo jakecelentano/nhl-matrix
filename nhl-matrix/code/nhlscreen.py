20e52609a449cb90c3f4aba299627173205f8f6f
Jake <jake.celentano@galleysolutions.co>
2023-03-09 11:39:39 -0500

    sleep less time to get updated records/date

--- nhl-matrix/code/nhlscreen.py
+++ nhl-matrix/code/nhlscreen.py
@@ -69,7 +69,7 @@ class NHLScreen(SampleBase):
                     print("Seconds until next game: " + str(seconds_until_next_game))
                     offscreen_canvas = self.getUpcomingGameScreen(game)
                     print("Drawing upcoming game screen")  
-                    sleep_time = max(seconds_until_next_game-300, 60)
+                    sleep_time = min(seconds_until_next_game-300, 7200)
 
                 offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas) 
             except KeyboardInterrupt:
